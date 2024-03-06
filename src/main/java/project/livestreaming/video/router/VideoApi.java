408046a57cfc6544c78a16fb00a691763506c336
HDPark95 <qkrgusen456@gmail.com>
2024-03-06 20:18:59 +0900

    [feat] 비디오 라우터 추가

---
+++ src/main/java/project/livestreaming/video/router/VideoApi.java
@@ -0,0 +1,8 @@
+package project.livestreaming.video.router;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoApi {
+
+}
---
+++ src/test/java/project/livestreaming/video/router/VideoApiTest.java
@@ -0,0 +1,13 @@
+package project.livestreaming.video.router;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebMvc
+class VideoApiTest {
+
+
+}
