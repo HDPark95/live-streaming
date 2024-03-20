9418875bd45cf43f1b91a0131dc669934a43d64c
HDPark95 <qkrgusen456@gmail.com>
2024-03-20 19:40:28 +0900

    [fetch] 엘라스틱 서치 엔티티 분리

--- src/main/java/project/livestreaming/search/domain/ESVideo.java
+++ src/main/java/project/livestreaming/search/domain/ESVideo.java
@@ -1,8 +1,20 @@
 package project.livestreaming.search.domain;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Document(indexName = "video")
+@NoArgsConstructor
 public class ESVideo {
+    private long videoId;
+    private String title;
+    private String description;
 
+    @Builder
+    public ESVideo(long videoId, String title, String description) {
+        this.videoId = videoId;
+        this.title = title;
+        this.description = description;
+    }
 }
