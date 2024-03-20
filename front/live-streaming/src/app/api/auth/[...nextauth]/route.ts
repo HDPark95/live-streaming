c644014720e2e6a57d997ad5faf31756fa93d4f3
dami325 <wnekfa1004@naver.com>
2024-03-20 20:34:15 +0900

    [feat] NextAuth 임시

--- front/live-streaming/src/app/api/auth/[...nextauth]/route.ts
+++ front/live-streaming/src/app/api/auth/[...nextauth]/route.ts
@@ -2,32 +2,46 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         CredentialsProvider({
-            name: 'Credentials',
+            name: 'credentials',
+            async authorize(credentials, req) {
+                console.log(credentials);
+                // const res = await fetch("/your/endpoint", {
+                //     method: 'POST',
+                //     body: JSON.stringify(credentials),
+                //     headers: { "Content-Type": "application/json" }
+                // })
+                //const user = await res.json()
+
+                // if (res.ok && user) {
+                //     return user
+                // }
+
+                let newVar = {
+                    id: '1',
+                    name: 'John Doe',
+                    email: 'n9qjv@example.com'
+                };
+                return newVar
+            },
             credentials: {
                 username: { label: "Username", type: "text", placeholder: "jsmith" },
                 password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials, req) {
-                const res = await fetch("/your/endpoint", {
-                    method: 'POST',
-                    body: JSON.stringify(credentials),
-                    headers: { "Content-Type": "application/json" }
-                })
-                const user = await res.json()
-
-                // If no error and we have user data, return it
-                if (res.ok && user) {
-                    return user
-                }
-                // Return null if user data could not be retrieved
-                return null
             }
         })
     ],
     callbacks: {
+        // async signIn({ user, account, profile, email, credentials }) {
+        //     console.log(user);
+        //   return true;
+        // },
+        async jwt({ token, user, account, profile, isNewUser }) {
+            return {...token, ...user}
+        },
         async session({ session, token }) {
+            console.log(123);
             return session;
         }
     }
--- front/live-streaming/src/component/login/LoginForm.tsx
+++ front/live-streaming/src/component/login/LoginForm.tsx
@@ -4,14 +4,15 @@ import { signIn, useSession } from 'next-auth/react'
 export default function LoginForm() {
     const handleOnSubmit = async (event: any) => {
         await signIn('credentials', {
+            callbackUrl: '/',
             username: event.target.username.value,
             password: event.target.password.value
         })
     }
     return (
             <form onSubmit={handleOnSubmit}>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
+                <input type="text" name="username" id="username" placeholder="Username" />
+                <input type="password" name="password" id="password" placeholder="Password" />
                 <button type="submit">Login</button>
             </form>
     );
